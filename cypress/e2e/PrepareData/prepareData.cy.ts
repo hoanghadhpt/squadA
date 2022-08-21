3503fcf04eb13c23e7a2a916dffdc93fd1d4c09a
Duyen Dao <105259715+duyendaodtt@users.noreply.github.com>
2022-08-21 15:47:14 +0700

    create branch prepare Data

---
+++ cypress/e2e/PrepareData/prepareData.cy.ts
@@ -0,0 +1,49 @@
+import queryPageContent from '../../PageObjects/query_pages';
+import ApiShareObjects from '../../PageObjects/apiSharedObjects';
+import APIPost from '../../PageObjects/apiSend';
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+})
+
+describe(`Prepare Data`, () => {
+    beforeEach(() => {})
+    const apiPost = new APIPost()
+    const apiExpected = new ApiShareObjects()
+    const testData = require("../../fixtures/sampleData.json");
+    describe("Create Data for Content Stack", () => {
+        testData.forEach((testDataRow : any) => {
+            const testData = {
+                contentType: testDataRow.contentType,
+                entryBody: testDataRow.entryData
+            };
+            context(`Create: ${
+                testData.contentType
+            }`, () => {
+                it(`GIVEN I create entry for ${
+                    testData.contentType
+                } `, () => {
+                    cy.fixture(testData.entryBody).then((data) => {
+                        apiPost.postCreateEntry(testData.contentType, data)
+                        cy.get('@resBody').then((data) => {
+                            cy.task('setStatus', data.status);
+                            cy.task('setBody', data.body)
+                        })
+                    })
+                })
+
+                it("Then I expected status code as 201", () => {
+                    apiExpected.verifyStatus(201)
+                })
+
+                it(`AND I publish ${testData.contentType}`, () => {
+                    cy.fixture(`/sampleData/entryUID/${testData.contentType}.txt`).then((data)=>{
+                        apiPost.postPublishUID(testData.contentType, data);
+                    })
+                })
+            })
+        })
+
+
+    })
+})
